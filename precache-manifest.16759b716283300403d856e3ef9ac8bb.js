e02065715b3aa1ca7b5bc68329225522c14da1a2
Jakub Tober <jakubtober276@gmail.com>
2022-12-26 19:40:15 +0100

    Updates

---
+++ precache-manifest.16759b716283300403d856e3ef9ac8bb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbbac0287c862ec5d838",
+    "url": "/Portfolio-page.github.io/static/css/main.6a1216d9.chunk.css"
+  },
+  {
+    "revision": "bbbac0287c862ec5d838",
+    "url": "/Portfolio-page.github.io/static/js/main.bbbac028.chunk.js"
+  },
+  {
+    "revision": "90d9f389e56d701cf965",
+    "url": "/Portfolio-page.github.io/static/js/1.90d9f389.chunk.js"
+  },
+  {
+    "revision": "d0ec17fecf89132b62d4",
+    "url": "/Portfolio-page.github.io/static/js/runtime~main.d0ec17fe.js"
+  },
+  {
+    "revision": "e65be0da8f7ec8230b00eeb667a0602c",
+    "url": "/Portfolio-page.github.io/static/media/Jakub_Tober_CV.e65be0da.pdf"
+  },
+  {
+    "revision": "6263707f10ad45bbe7e69b32a883efde",
+    "url": "/Portfolio-page.github.io/static/media/me.6263707f.png"
+  },
+  {
+    "revision": "127b470a1814ca2ae06d9f40df34f190",
+    "url": "/Portfolio-page.github.io/static/media/reakt.127b470a.png"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/Portfolio-page.github.io/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/Portfolio-page.github.io/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/Portfolio-page.github.io/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/Portfolio-page.github.io/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/Portfolio-page.github.io/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/Portfolio-page.github.io/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "025327af5cc8a89ea1e44e025adc2ef3",
+    "url": "/Portfolio-page.github.io/index.html"
+  }
+];
